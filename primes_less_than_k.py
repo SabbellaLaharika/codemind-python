400584975273e464a357a00f41c74c5ee4dd99a9
SabbellaLaharika <119842850+SabbellaLaharika@users.noreply.github.com>
2023-05-21 19:16:51 +0530

    def isprime(n) :     cnt = 0     for i in range(1,n) :         if n % i == 0 :             cnt += 1     if cnt == 1 :         return 1     else :         return 0 n = int(input()) l = list(map(int, input().split())) k = int(input()) count = 0 for i in l :     if isprime(i) :         if i <= k :             count += 1 print(count)

---
+++ primes_less_than_k.py
@@ -0,0 +1,18 @@
+def isprime(n) :
+    cnt = 0
+    for i in range(1,n) :
+        if n % i == 0 :
+            cnt += 1
+    if cnt == 1 :
+        return 1
+    else :
+        return 0
+n = int(input())
+l = list(map(int, input().split()))
+k = int(input())
+count = 0
+for i in l :
+    if isprime(i) :
+        if i <= k :
+            count += 1
+print(count)
